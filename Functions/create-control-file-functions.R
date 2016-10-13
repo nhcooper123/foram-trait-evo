44b1188f2a3994b45cd2762fd8fabc2820395559
Natalie Cooper <natalie.cooper@nhm.ac.uk>
2016-10-13 15:24:10 +0100

    Remove string creating functions as they are not needed/don't work

--- Functions/create-control-file-functions.R
+++ Functions/create-control-file-functions.R
@@ -1,34 +1,25 @@
 # Functions to create flexible control files for BAMM analyses
 
-# For a given row, create a string in the form "variable name = value,"
-create.parameters.string <- function(combinations.row) {
-  # Create empty string
-  params <- c() 
-  # For each variable, create string in form of "variable name = value,"
-  for(i in seq_along(names(combinations.row))) {
-    params <- str_c(params, names(combinations.row)[i], " = ", 
-                    combinations.row[[i]], ", ", sep = "")
-  } 
-  # Remove extra comma at end of string   
-  params <- str_sub(params, end = -3) 
-  # Output the string with parameter combinations for the row
-  return(params)
+# Get all possible parameter combinations from an input list of possible values
+all.combinations <- function(...){
+  # Get list of variable names entered into function
+  # This may vary depending on what we want to enter
+  # May need to modify to add tree file and other vital variables to make requirment???####
+  variables <- substitute(list(...))[-1]
+  var.names <- sapply(variables, deparse)
+
+  # Use expand.grid to get all combinations of parameters possible and create dataframe
+  # Name using input names
+  all.options <- expand.grid(...)
+  names(all.options) <- var.names
+  
+  return(all.options)
 }
 
-# Create strings in the form "variable name = value," for whole dataframe 
-# and add to parameters column in the dataframe
-create.parameters.column <- function(combinations.dataframe) {
-  # Extract number of rows in dataframe
-  nrows <- nrow(combinations.dataframe)
-  # Create empty column to fill with parameter combinations
-  parameters <- array(dim = c(nrows,1))
-  # Loop through each row to create and store parameter combinations
-  for(row.number in 1:nrows) {
-    row <- slice(combinations.dataframe, row.number)
-    parameters[row.number,] <- create.parameters.string(row)
-  }
-  # Put parameters into the dataframe and return
-  combinations.dataframe$parameters <- parameters
-  return(combinations.dataframe)         
-}
+numberOfGenerations <- seq(10000, 15000, by = 1000)
+observationTime <- c(100, 200, 350)
 
+qq <- all.combinations(observationTime, numberOfGenerations)
+
+generateControlFile_fossils(file = 'control.txt', type = 'diversification',
+                            params = as.list(qq[18,1:2]))
