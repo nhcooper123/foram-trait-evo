845b9629055c5c22bb17271fe18208b5b5285e3f
Natalie Cooper <ncooper@tcd.ie>
2016-10-14 12:40:04 +0100

    Completed outputs for full function that creates all required control files

--- Functions/create-control-file-functions.R
+++ Functions/create-control-file-functions.R
@@ -1,5 +1,5 @@
 # Functions to create multiple control files for BAMM analyses
-# Requires 
+# Requires generateControlFile_fossils.R
 # Natalie Cooper
 
 # Get all possible parameter combinations from an input list of possible values
@@ -13,29 +13,65 @@ all.parameter.combinations <- function(...) {
   return(all.options)
 }
 
-# Input is a list of variables already defined for entry into the control file
-# Requires treefile, the rest are optional
-# Names must match those in the control file template
-# type = "diversification" or "trait"
-all.control.files <- function(..., type = type) {
-  # Create dataframe with all possible parameter combinations
-  combinations <- all.parameter.combinations(...)
-  # Extract number of rows (combinations) and columns (options being changed)
-  nrows <- nrow(combinations)
-  nvars <- ncol(combinations)
-  # Loop through each combination to create control files for each
-  for(i in 1:nrows){
-    # Need to name control files sensibly and/or place in separate folder
-    filename <- "control.txt"
-    generateControlFile_fossils(file = filename, type = type,
-                                params = as.list(c(combinations[i, 1:nvars]), 
-                                                 outName = filename)
-    write.table(control.files.txt) # Create a table of all the options in each file? Migth be easier than naming
-  }
+# Create file names for each control file
+name.control.file <- function(prefix, row) {
+  paste(prefix, "_", row, ".txt", sep = "")
 }
 
-# Example input
-# numberOfGenerations <- seq(10000, 15000, by = 1000)
-# observationTime <- c(100, 200, 350)
-# treefile <- c("whale.tre")
+# Create new dataframe with column to add names of control files
+build.control.details <- function(combinations) {
+  nrows <- nrow(combinations)
+  control.details <- combinations
+  control.details$control.file.name <- rep("NA", nrows)
+  return(control.details)
+}
 
+# Add names of control files to column in dataframe
+add.control.details <- function(control.details, row, filename) {
+  control.details$control.file.name[row] <- filename
+  return(control.details)
+}
+
+# Create control files for all parameter combinations
+# Input names must match those in the control file template
+# type = "diversification" or "trait"
+# prefix and suffix should aid identification of batches
+# prefix is added to each control file name
+# suffix is added to the end of the control file details file name
+# Output files will go to your working directory
+all.control.files <- function(type = type, prefix = "control", suffix = "", ...) {
+  
+  # Create dataframe with all possible parameter combinations
+  combinations <- all.parameter.combinations(...)
+  
+  # Create dataframe to enter details of control files and their names
+  control.details <- build.control.details(combinations)
+  
+  # Extract number of rows (parameter combinations) and columns (parameters being changed)
+  nrows <- nrow(combinations)
+  nvars <- ncol(combinations)
+
+  # Loop through each combination to create control files for each
+  for(i in 1:nrows){
+    # Name control files and add name to control file details dataframe
+    filename <- name.control.file(prefix, i)
+    control.details <- add.control.details(control.details, i, filename)
+    # Generate the control file
+    generateControlFile_fossils(file = filename, type = type,
+                                params = as.list(c(combinations[i, 1:nvars], 
+                                                 outName = filename))) 
+  }
+  
+  # Output the list of control file names and details
+  write.table(file = paste("controlfiles_", suffix, ".txt", sep = ""), control.details, sep = "\t", row.names = FALSE, 
+              col.names = TRUE, quote = FALSE) 
+}
+
+# Example of usage 
+# 1 - Input parameters you want to tweak and range of values to try
+# numberOfGenerations <- seq(10000, 15000, by = 1000)
+# observationTime <- c(100, 200)
+# treefile <- c("whale.tre", "whale_resolved.tre")
+# 2 - Create control files
+# all.control.files(type = "diversification", prefix = "control", suffix = "Oct16", 
+#                   treefile, observationTime, numberOfGenerations)
